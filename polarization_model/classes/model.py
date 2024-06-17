abcd16ac6bc7b7dc162de4fb71be76bad59519d6
Koekoek1337 <hajo-groen@mail.com>
2024-06-17 22:28:04 +0200

    Documentation

--- polarization_model/classes/model.py
+++ polarization_model/classes/model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from agent import PolarizationAgent
 
-from typing import Any
+from typing import Any, Tuple
 
 
 OPINION_A = -1
@@ -12,6 +12,7 @@ OPINION_B = 1
 
 class PolarizationModel(mesa.Model):
     """
+    TODO: Decide on sampling method for random agents
 
     """
     def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -30,14 +31,22 @@ class PolarizationModel(mesa.Model):
         """Amount of agents present in the model (source of unique ID's)"""
 
         self.opinionA = OPINION_A
+        """Extreme value for opinion A"""
         self.opinionB = OPINION_B
-        self.opinionDist = np.abs(OPINION_A) + np.abs(OPINION_B)
+        """Extreme value for opinion B"""
+        self.opinionDist = np.abs(self.opinionA) + np.abs(self.opinionB)
+        """Absolute distance between opinions A and B"""
 
-    def addAgent(self, opinion: float, conformity: float ,tolerance: float):
+    def addAgent(self, opinion: float, conformity: float, tolerance: float) -> None:
         """
         # TODO: Testing
         
         Add an agent to the model and scheduler
+        
+        Args:
+            opinion:    (float) The initial opinion value for the agent
+            conformity: (float) The fixed conformity value for the agent
+            tolerance:  (float) The fixed tolerance value for the agent
         """
         self.graph.add_node(self.nAgents)
         newAgent = PolarizationAgent(self.nAgents, self, opinion, conformity, tolerance)
