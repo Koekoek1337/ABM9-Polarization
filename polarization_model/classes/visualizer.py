b0a72ea7782784e472da827ea571b37908e3e767
Hajo <106412846+Koekoek1337@users.noreply.github.com>
2024-06-23 18:27:14 +0200

    Visualizer

--- polarization_model/classes/visualizer.py
+++ polarization_model/classes/visualizer.py
@@ -6,25 +6,44 @@ from .agent import PolarizationAgent
 from .model import PolarizationModel
 
 class Visualizer():
-    def __init__(self, model: PolarizationModel) -> None:
+    def __init__(self, model: PolarizationModel, bins = 9) -> None:
         self.model        = model
+        """PolarizationModel"""
         self.graph        = model.graph
-        self.pos          = nx.spring_layout(self.graph)
-        self.fig, ax      = plt.subplots()
-        assert isinstance(ax, plt.Axes)
-        self.ax = ax
+        """Networkx Graph instance of polarization model"""
+        self.fig, axes    = plt.subplots(1, 2)
+        axNx, axDist      = axes
 
-        self.text = self.ax.text(0.8, 0.9, f"i = {0}")
+        assert isinstance(axNx,   plt.Axes)
+        assert isinstance(axDist, plt.Axes)
+
+        self.bins         = bins
+        """Amount of bins for opinion Histogram"""
+
+        self.axNx   = axNx
+        """Axes instance for graph plot"""
+        self.axDist = axDist
+        """Axes instance for distribution plot"""
+
+        self.text = self.axNx.text(0.8, 0.9, f"i = {0}")
+        self._opinions = self.model.agentOpinions()
     
         self.drawNetworkx()
     
     def drawNetworkx(self):
+        self.axNx.clear()
         cmap = []
-        for nodeID, opinion in zip(list(range(self.model.nAgents)), self.model.agentOpinions()):
+        for nodeID, opinion in zip(list(range(self.model.nAgents)), self._opinions):
             color = self.colorpicker(opinion)
             cmap.append(color)
 
-        nx.draw_circular(self.graph, node_color=cmap, ax=self.ax)
+        nx.draw_circular(self.graph, node_color=cmap, ax=self.axNx, node_size=20)
+    
+    def drawDist(self):
+        self.axDist.clear()
+        self.axDist.hist(self._opinions, self.bins, range=(-1,1), density=True)
+        self.axDist.set_xbound(-1, 1)
+        # self.axDist.set_ybound(0, 1)
 
     def colorpicker(self, opinion):
         if opinion < 0.0:  return "Blue"
@@ -32,11 +51,12 @@ class Visualizer():
         else: return "Grey"
 
     def _frame(self, framenum):
-        self.ax.clear()
+        self._opinions = self.model.agentOpinions()
         self.drawNetworkx()
+        self.drawDist()
         self.model.step()
 
-        self.text = self.ax.text(0.8, 0.9, f"i = {framenum}")
+        self.text = self.axNx.text(0.8, 0.9, f"i = {framenum}")
         return
     
     def run(self, nFrames = 100, fps = 1):
