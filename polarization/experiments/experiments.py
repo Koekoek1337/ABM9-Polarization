e8b5dc04451f4feee0ddb5ffbc3dfe913ddfe0d9
Archie <qizhangedu@gmail.com>
2024-06-26 15:00:06 +0200

    fix tuple

--- polarization/experiments/experiments.py
+++ polarization/experiments/experiments.py
@@ -6,7 +6,7 @@
 
 experiments = [
     # default               sl  rho  m  a  b   sf  c  ox  ht
-    {"name":"default_small",            "values":[10, 0.8, 2, 4, 1, 0.8, 5, 2, 0.8]},
+    # {"name":"default_small",            "values":[10, 0.8, 2, 4, 1, 0.8, 5, 2, 0.8]},
     # {"name":"default_small_dens",       "values":[10, 0.9, 2, 4, 1, 0.8, 5, 2, 0.8]},
     # {"name":"default_average",          "values":[20, 0.8, 2, 4, 1, 0.8, 5, 2, 0.8]},
     # {"name":"default_average_dens",     "values":[20, 0.9, 2, 4 ,1, 0.8, 5, 2, 0.8]},  # MAIN COMPARIS}N
@@ -15,7 +15,7 @@ experiments = [
     # {"name":"default_extra_large_dens", "values":[40, 0.95,2, 4, 1, 0.8, 5, 2, 0.8]},
     # {"name":"tolerance_high",           "values":[20, 0.9, 2, 4, 1, 0.8, 5, 4, 0.8]},
     # {"name":"tolerance_low",            "values":[20, 0.9, 2, 4, 1, 0.8, 5, 1, 0.8]},
-    # {"name":"alpha_low",                "values":[20, 0.9, 2, 1, 1, 0.8, 5, 2, 0.8]},
+    {"name":"alpha_low",                "values":[20, 0.9, 2, 1, 1, 0.8, 5, 2, 0.8]},
     # {"name":"alpha_high",               "values":[20, 0.9, 2, 6, 1, 0.8, 5, 2, 0.8]},
     # {"name":"beta_high",                "values":[20, 0.9, 2, 4, 3, 0.8, 5, 2, 0.8]},
 ]
